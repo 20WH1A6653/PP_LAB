baeffb89f53784ee0538100eb7d6def89be751da
20WH1A6653 <93580510+20WH1A6653@users.noreply.github.com>
2021-11-04 15:35:22 +0530

    Update binary_recursion.py

--- binary_recursion.py
+++ binary_recursion.py
@@ -1,12 +1,12 @@
 def binary_search(arr,n,low,high,key):
     mid = (low + high) // 2
-    if(low > high)
-       Return 0
+    if(low > high):
+       return 0
     if(arr[mid] == key):
       return mid+1
     elif(arr[mid] < key):
       low = mid+1
-    elif(arr[mid > key):
+    elif(arr[mid] > key):
       high = mid-1
     else:
       return binary_search(arr,n,low,high,key)
@@ -21,8 +21,11 @@ print(arr)
 key = int(input("enter search key"))
 ans = binary_search(arr,n,0,n-1,key)
 if(ans!=0):
- print("element found at position"ans)
+ print("element found at position",ans)
 else:
- print("element not found)
+ print("element not found")
+
+
+
 
 
